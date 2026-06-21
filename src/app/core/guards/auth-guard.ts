a8145d683fe005dfe20767372de96e09431dfc03
IT24104152 <gayuthdemika@gmail.com>
2026-06-21 07:30:53 +0530

    feat: auth feature on the frontend

---
+++ src/app/core/guards/auth-guard.ts
@@ -0,0 +1,12 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const apiKey = localStorage.getItem('api_key');
+  if (apiKey) {
+    return true;
+  }
+  router.navigate(['/login']);
+  return false;
+};
