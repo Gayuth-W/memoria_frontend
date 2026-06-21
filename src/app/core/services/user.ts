f61ce90c389df5934a3b39e2c0cce019dd17e689
IT24104152 <gayuthdemika@gmail.com>
2026-06-21 07:34:03 +0530

    feat: user service

---
+++ src/app/core/services/user.ts
@@ -0,0 +1,13 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/users`;
+
+  create(apiKey: string) {
+    return this.http.post(this.apiUrl, { api_key: apiKey });
+  }
+}
