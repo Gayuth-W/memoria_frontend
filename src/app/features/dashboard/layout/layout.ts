e9e8e886b9ef61786b378024e41f9a52787e0b61
IT24104152 <gayuthdemika@gmail.com>
2026-06-22 12:17:48 +0530

    feat: dashboard structure

---
+++ src/app/features/dashboard/layout/layout.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css'
+})
+export class Layout {
+  private authService = inject(AuthService);
+
+  logout() {
+    this.authService.logout();
+  }
+}
